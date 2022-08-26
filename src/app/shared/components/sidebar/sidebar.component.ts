8712c2c7cfbb1c3d7995aacab6f85dd463f3c646
Jhonatan Acero <jhoanatansaa90@hotmail.com>
2022-08-26 12:05:01 -0500

    add husky package.json

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-@Component({
+@Component( {
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: [
   ]
-})
-export class SidebarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+} )
+export class SidebarComponent { }
