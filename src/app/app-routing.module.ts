f227f3b963e96a72de55848aee1077b11a3a0516
Jhonatan Acero <jhoanatansaa90@hotmail.com>
2022-08-26 07:35:00 -0500

    First commit test

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ByCapitalComponent } from "@country/pages/by-capital/by-capital.component";
+import { ByCountryComponent } from "@country/pages/by-country/by-country.component";
+import { ByRegionComponent } from "@country/pages/by-region/by-region.component";
+import { SeeCountryComponent } from "@country/pages/see-country/see-country.component";
+
+const ROUTES: Routes = [
+  { path: '', component: ByCountryComponent, pathMatch: 'full' },
+  { path: 'region', component: ByRegionComponent },
+  { path: 'capital', component: ByCapitalComponent },
+  { path: 'country/:id', component: SeeCountryComponent },
+  { path: '**', redirectTo: '' },
+]
+
+@NgModule( {
+  imports: [ RouterModule.forRoot( ROUTES, { useHash: true } ) ],
+  exports: [ RouterModule ]
+} )
+export class AppRoutingModule
+{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { SharedModule } from '@shared/shared.module';
+import { CountryModule } from '@country/country.module';
 
-@NgModule({
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule( {
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    CountryModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
-})
+  bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
